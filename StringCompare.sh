121eb5a30458cba4e30dca69e14d5075dcdf126d
Liyansu <69055989+Liyansu@users.noreply.github.com>
2021-06-02 18:27:44 +0530

    Create StringCompare.sh

---
+++ StringCompare.sh
@@ -0,0 +1,9 @@
+Sourcesystem="ABC"
+if [ 'XYZ' -eq '$Sourcesystem' ]; 
+then 
+    echo "Sourcesystem Matched" 
+else
+    echo "Sourcesystem is NOT Matched $Sourcesystem"  
+fi;
+
+echo Sourcesystem Value is  $Sourcesystem ;
